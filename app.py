a348a92693911a00adf618f8fb7c57503459b3e5
Mikołaj Połonowicz <65428411+mccxvi@users.noreply.github.com>
2022-10-30 17:57:40 +0100

    - Account creation, + Game Code
    - Doesn't require account creation or setting up admin account.
    - Added game code system in which the code is generated on every run instance of the app.py

--- app.py
+++ app.py
@@ -1,150 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#/////////////////////////////////////////
+
+# 🠺 07/04/2022 10:50:02
+#
+# ↳ Author 🡢 Mikołaj Połonowicz @ MCCXVI
+# ↳ https://github.com/mccxvi/Pokemon-Crowd-Control-Flask/
+
+#/////////////////////////////////////////
+
+# Load modules to use
 from flask import Flask, render_template, Response, request, redirect, url_for, flash, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length, Regexp, ValidationError
 from flask_bcrypt import Bcrypt
 from pynput.keyboard import Listener, KeyCode, Key, Controller as KeyboardController
 import time
+import sys
 import logging
+import string
+import random
 
-# Turn off logging POST/GET messages in terminal
+# List holding users already playing / logged in
+already_logged_in = []
+
+# Turning off logging of http requests etc.
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+# Set pynput keyboard controller
 keyboard = KeyboardController()
 
-# Defining Keys
+# Creating game code function
+def keygen(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
-up_button = Key.up
-down_button = Key.down
-left_button = Key.left
-right_button = Key.right
-a_button = Key.backspace
-b_button = Key.space
-start_button = Key.home
-select_button = Key.delete
+# Find playing user function
+def search_user(list, platform):
+    for i in range(len(list)):
+        if list[i] == platform:
+            return True
+    return False
 
-# App Flask Config
+# Showing active game code in console
+gamekey = keygen()
+print(f"Key for the current game is: {gamekey}")
+
+# Loading Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
-# Login Session Manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "authsesh"
-
-@login_manager.user_loader
-def load_user(user_id):
-    return users.query.get(int(user_id))
-
-# Models for authentication
-class users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(12))
-    authkey = db.Column(db.String(80))
-
-class AddUsers(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=12)], render_kw={"placeholder": "Username"})
-    authkey = PasswordField(validators=[InputRequired(), Length(min=8, max=32)], render_kw={"placeholder": "Auth Key"})
-
-    submit = SubmitField("Add User")
-
+# Login user input
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=12)], render_kw={"placeholder": "Username"})
-    authkey = PasswordField(validators=[InputRequired(), Length(min=8, max=32)], render_kw={"placeholder": "Auth Key"})
+    authkey = StringField(validators=[InputRequired(), Length(min=6, max=6)], render_kw={"placeholder": "Game Key"})
 
-    submit = SubmitField("Login")
+    submit = SubmitField("Play")
 
-# Register Page
-@app.route('/addusers', methods=['POST', 'GET'])
-@login_required
-def addusers():
-#    if session['user'] != "YOUR_ADMIN_NAME":
-#        return redirect("https://www.youtube.com/watch?v=ixMHG0DIAK4", code=302)
-#    else:
-    form = AddUsers()
-    if form.validate_on_submit():
-        hashed_authkey = bcrypt.generate_password_hash(form.authkey.data)
-        new_user = users(username=form.username.data, authkey=hashed_authkey)
-        db.session.add(new_user)
-        db.session.commit()
-
-    return render_template("addusers.html", form=form)
-
-# Login Page
+# Login form page
 @app.route('/auth', methods=['POST', 'GET'])
 def authsesh():
-    if current_user.is_authenticated:
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = request.form['username']
+        if search_user(already_logged_in, user):
+            return redirect(url_for('authsesh'))
+        else:
+            if gamekey == request.form['authkey']:
+                session['user'] = request.form['username']
+                already_logged_in.append(user)
+                return redirect(url_for('game'))
+
+    if session.get('user') is not None:
         return redirect(url_for('game'))
-    else:
-        form = LoginForm()
-        if form.validate_on_submit():
-            user = users.query.filter_by(username=form.username.data).first()
-            if user:
-                    if bcrypt.check_password_hash(user.authkey, form.authkey.data):
-                        login_user(user)
-                        session['user'] = request.form['username']
-                        return redirect(url_for('game'))
 
     return render_template("auth.html", form=form)
 
-# Main Game
+# Main game page
 @app.route("/", methods=['POST', 'GET'])
-@login_required
 def game():
+    if session.get('user') is None:
+        return redirect(url_for('authsesh'))
+    else:
+        return render_template('index.html', user=session['user'])
+
     return render_template('index.html', user=session['user'])
 
-# AJAX Post for movement update and pressing buttons
+# Update page
+# On users button press HTTP POST requests goes here, gets processed by pynput
+# as a keyboard input and tells ajax to stay on the same site (just refresh on update)
 @app.route("/update", methods=['POST', 'GET'])
-@login_required
 def update():
-    button_value = request.form['button_value']
+    try:
+        if session.get('user') is None:
+            return redirect(url_for('game'))
+        else:
+            button_value = request.form['button_value']
+            playing_user = session['user']
+            print(f"{session['user']} pressed '{button_value}' Button")
+            flash(f"You Have Pressed '{button_value}'")
 
-    print(f"{session['user']} pressed '{button_value}' Button")
-    flash(f"You Have Pressed '{button_value}'")
+            if button_value == "Up":
+                up_button = Key.up
+                keyboard.press(up_button)
+                time.sleep(0.2)
+                keyboard.release(up_button)
+            elif button_value == "Down":
+                down_button = Key.down
+                keyboard.press(down_button)
+                time.sleep(0.2)
+                keyboard.release(down_button)
+            elif button_value == "Left":
+                left_button = Key.left
+                keyboard.press(left_button)
+                time.sleep(0.2)
+                keyboard.release(left_button)
+            elif button_value == "Right":
+                right_button = Key.right
+                keyboard.press(right_button)
+                time.sleep(0.2)
+                keyboard.release(right_button)
+            elif button_value == "A":
+                a_button = Key.backspace
+                keyboard.press(a_button)
+                time.sleep(0.1)
+                keyboard.release(a_button)
+            elif button_value == "B":
+                b_button = Key.space
+                keyboard.press(b_button)
+                time.sleep(0.1)
+                keyboard.release(b_button)
+            elif button_value == "Start":
+                start_button = Key.home
+                keyboard.press(start_button)
+                time.sleep(0.1)
+                keyboard.release(start_button)
+            elif button_value == "Select":
+                select_button = Key.delete
+                keyboard.press(select_button)
+                time.sleep(0.1)
+                keyboard.release(select_button)
 
-    if button_value == "Up":
-        keyboard.press(up_button)
-        time.sleep(0.3)
-        keyboard.release(up_button)
-    elif button_value == "Down":
-        keyboard.press(down_button)
-        time.sleep(0.3)
-        keyboard.release(down_button)
-    elif button_value == "Left":
-        keyboard.press(left_button)
-        time.sleep(0.3)
-        keyboard.release(left_button)
-    elif button_value == "Right":
-        keyboard.press(right_button)
-        time.sleep(0.3)
-        keyboard.release(right_button)
-    elif button_value == "A":
-        keyboard.press(a_button)
-        time.sleep(0.1)
-        keyboard.release(a_button)
-    elif button_value == "B":
-        keyboard.press(b_button)
-        time.sleep(0.1)
-        keyboard.release(b_button)
-    elif button_value == "Start":
-        keyboard.press(start_button)
-        time.sleep(0.1)
-        keyboard.release(start_button)
-    elif button_value == "Select":
-        keyboard.press(select_button)
-        time.sleep(0.1)
-        keyboard.release(select_button)
+            return jsonify({'result' : 'success'})
+        
+    except:
+        return redirect(url_for('game'))
 
-    return jsonify({'result' : 'success'})
-
-# Run Server
+# Run flask web app
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True, host="YOUR_IP")
